bafdd0705bfc399bf9ae24521da83fffc6ac3cfd
Sam Kern <samkern108@gmail.com>
2017-12-14 16:26:00 -0800

    Audio stuff missing from the last commit. Slightly better UI.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -9,19 +9,12 @@ public class UIManager : MonoBehaviour, IRestartObserver {
 	public static UIManager instance;
 
 	private int exitsCompleted = 0;
-	private float startTime = 0f;
 
 	private bool stop = false;
 
 	public void Start() {
 		instance = this;
-		startTime = Time.time;
 		NotificationMaster.restartObservers.Add (this);
-	}
-
-	public void Update() {
-		if(!stop)
-			timerText.text = "" + (Time.time - startTime);
 	}
 
 	public void Stop() {
@@ -36,8 +29,5 @@ public class UIManager : MonoBehaviour, IRestartObserver {
 	public void Restart() {
 		exitsCompleted = 0;
 		exitsText.text = "" + exitsCompleted;
-
-		startTime = Time.time;
-		timerText.text = "0";
 	}
 }
