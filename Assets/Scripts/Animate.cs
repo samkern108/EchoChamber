e2ece5eb6b20f16e6056211d09f5021bbb8a1315
Sam Kern <samkern108@gmail.com>
2017-12-31 16:23:32 -0800

    Animate and notification bug cleanup. It's gross but functional.

--- Assets/Scripts/Animate.cs
+++ Assets/Scripts/Animate.cs
@@ -6,19 +6,23 @@ using MovementEffects;
 public class Animate : MonoBehaviour {
 
 	private SpriteRenderer spriteRenderer;
+	private static int tagCounter = 0;
+	private string tag;
 
 	public void Awake() {
 		spriteRenderer = GetComponent <SpriteRenderer>();
+		tag = tagCounter + "";
+		tagCounter++;
 	}
 
 	// SIZE
 
 	public void AnimateToSize(Vector2 start, Vector2 finish, float t) {
-		Timing.RunCoroutine (C_AnimateToSizeAndBack(start, finish, t, false));
+		Timing.RunCoroutine (C_AnimateToSizeAndBack(start, finish, t, false), tag);
 	}
 
 	public void AnimateToSizeAndBack(Vector2 start, Vector2 finish, float t) {
-		Timing.RunCoroutine (C_AnimateToSizeAndBack(start, finish, t, true));
+		Timing.RunCoroutine (C_AnimateToSizeAndBack(start, finish, t, true), tag);
 	}
 
 	private IEnumerator<float> C_AnimateToSizeAndBack (Vector2 start, Vector2 finish, float duration, bool goBack) {
@@ -30,18 +34,18 @@ public class Animate : MonoBehaviour {
 			yield return 0;
 		}
 		if(goBack) {
-			Timing.RunCoroutine (C_AnimateToSizeAndBack(finish, start, duration, false));
+			Timing.RunCoroutine (C_AnimateToSizeAndBack(finish, start, duration, false), tag);
 		}
 	}
 
 	// COLOR
 
 	public void AnimateToColor(Color start, Color finish, float t) {
-		Timing.RunCoroutine (C_AnimateToColorAndBack(start, finish, t, false));
+		Timing.RunCoroutine (C_AnimateToColorAndBack(start, finish, t, false), tag);
 	}
 
 	public void AnimateToColorAndBack(Color start, Color finish, float t) {
-		Timing.RunCoroutine (C_AnimateToColorAndBack(start, finish, t, true));
+		Timing.RunCoroutine (C_AnimateToColorAndBack(start, finish, t, true), tag);
 	}
 
 	private IEnumerator<float> C_AnimateToColorAndBack (Color start, Color finish, float duration, bool goBack) {
@@ -53,11 +57,11 @@ public class Animate : MonoBehaviour {
 			yield return 0;
 		}
 		if(goBack) {
-			Timing.RunCoroutine (C_AnimateToColorAndBack(finish, start, duration, false));
+			Timing.RunCoroutine (C_AnimateToColorAndBack(finish, start, duration, false), tag);
 		}
 	}
 
 	void OnDestroy() {
-		StopAllCoroutines ();
+		Timing.KillCoroutines (tag);
 	}
 }
--- Assets/Scripts/NotificationMaster.cs
+++ Assets/Scripts/NotificationMaster.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public interface IRestartObserver
 {
@@ -27,6 +28,7 @@ public interface IGhostDeathObserver
 	void GhostDied(GhostAIStats ghost);
 }
 
+// TODO(samkern): This absolutely doesn't scale. Go figure out how to make this better.
 public class NotificationMaster : MonoBehaviour {
 
 	public static List<IRestartObserver> restartObservers = new List<IRestartObserver> ();
@@ -35,33 +37,73 @@ public class NotificationMaster : MonoBehaviour {
 	public static List<IGhostDeathObserver> ghostDeathObservers = new List<IGhostDeathObserver> ();
 	public static List<IPlayerShootObserver> playerShootObservers = new List<IPlayerShootObserver> ();
 
+	/* TODO(samkern): Possible simplification? go learn about delegates.
+	public delegate void RestartDelegate();
+	public static List<RestartDelegate> restartDelegates = new List<RestartDelegate>();
 	public static void SendRestartNotification() {
+		foreach (RestartDelegate del in restartDelegates) {
+			del ();
+		}
+	}*/
+
+	public static void SendRestartNotification() {
+		List<IRestartObserver> toRemove = new List<IRestartObserver> ();
+		foreach(IRestartObserver o in restartObservers) {
+			if (o.Equals (null))
+				toRemove.Add (o);
+			else
+				o.Restart ();
+		}
+		restartObservers = restartObservers.Except (toRemove).ToList();
+	}
+
+	/*public static void SendRestartNotification() {
 		foreach(IRestartObserver o in restartObservers) {
 			o.Restart ();
 		}
-	}
+	}*/
 
 	public static void SendPlayerDeathNotification() {
+		List<IPlayerObserver> toRemove = new List<IPlayerObserver>();
 		foreach(IPlayerObserver o in playerObservers) {
-			o.PlayerDied ();
+			if (o.Equals (null))
+				toRemove.Add (o);
+			else
+				o.PlayerDied ();
 		}
+		playerObservers = playerObservers.Except (toRemove).ToList ();
 	}
 
 	public static void SendPlayerShootNotification() {
+		List<IPlayerShootObserver> toRemove = new List<IPlayerShootObserver> ();
 		foreach(IPlayerShootObserver o in playerShootObservers) {
-			o.PlayerShoot ();
+			if (o.Equals (null))
+				toRemove.Add (o);
+			else
+				o.PlayerShoot ();
 		}
+		playerShootObservers = playerShootObservers.Except (toRemove).ToList ();
 	}
 
 	public static void SendCheckpointReachedNotification(float timeOpen) {
+		List<ICheckpointObserver> toRemove = new List<ICheckpointObserver> ();
 		foreach(ICheckpointObserver o in checkpointObservers) {
-			o.CheckpointActivated (timeOpen);
+			if (o.Equals (null))
+				toRemove.Add (o);
+			else
+				o.CheckpointActivated (timeOpen);
 		}
+		checkpointObservers = checkpointObservers.Except (toRemove).ToList ();
 	}
 
 	public static void SendGhostDeathNotification(GhostAIStats stats) {
+		List<IGhostDeathObserver> toRemove = new List<IGhostDeathObserver> ();
 		foreach(IGhostDeathObserver o in ghostDeathObservers) {
-			o.GhostDied (stats);
+			if (o.Equals (null))
+				toRemove.Add (o);
+			else
+				o.GhostDied (stats);
 		}
+		ghostDeathObservers = ghostDeathObservers.Except (toRemove).ToList ();
 	}
 }
