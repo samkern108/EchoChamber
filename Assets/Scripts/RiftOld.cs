c98847f9a3f1961aee738b3c4c1948e399d86217
Sam Kern <samkern108@gmail.com>
2018-01-11 21:52:58 -0800

    Sooo yeah, made a particle system for the rift instead of a flat sprite. The sprite was sad and I didn't like it.

--- Assets/Scripts/RiftOld.cs
+++ Assets/Scripts/RiftOld.cs
@@ -3,36 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using MovementEffects;
 
-public class Rift : MonoBehaviour, IRestartObserver, IGhostDeathObserver, IPlayerShootObserver {
+public class RiftOld : MonoBehaviour, IRestartObserver, IGhostDeathObserver, IPlayerShootObserver {
 
 	GhostAIStats ghostStats;
-	ParticleSystem ps;
-	BoxCollider2D boxCollider;
 
 	// TODO(samkern): Should rifts get bigger over time? Spawn little enemies? How should we close them?
 	// TODO(samkern): Idea: maybe if shit's really hitting the fan, a checkpoint has a random chance of spawning a friendly ghost? Or like, if you catch it really early it becomes friendly?
 
 	private bool activated = false;
 
+	private float size = 0.08f;
 	private float timeOpened;
 
-	private AnimationCurve psSizeCurve;
+	//private Animate animate;
 
 	public void Start() {
 		timeOpened = Time.time;
 
 		ghostStats = new GhostAIStats ();
 
-		ps = GetComponent <ParticleSystem>();
-		boxCollider = GetComponent <BoxCollider2D>();
-
 		NotificationMaster.restartObservers.Add (this);
 		NotificationMaster.ghostDeathObservers.Add (this);
 		NotificationMaster.playerShootObservers.Add (this);
 
-		psSizeCurve = new AnimationCurve();
-		psSizeCurve.AddKey(0.0f, 0.0f);
-		psSizeCurve.AddKey(1.0f, 1.0f);
+		//animate = GetComponent <Animate>();
 
 		Vector3 point;
 		float distance, minDistance;
@@ -48,25 +42,14 @@ public class Rift : MonoBehaviour, IRestartObserver, IGhostDeathObserver, IPlaye
 		StartCoroutine ("C_AnimateSize");
 	}
 		
-	private float psRadius = 0.0f;
-	private float endSize = .0f;
 	private IEnumerator C_AnimateSize () {
 		Vector3 newSize;
 		float delay;
 		while(true) {
-			var shape = ps.shape;
-
-			psRadius += .05f;
-			endSize += .1f;
-
-			shape.radius = psRadius;
-
-			var size = ps.sizeOverLifetime;
-			boxCollider.size = new Vector2 (psRadius, psRadius);
-
-			size.size = new ParticleSystem.MinMaxCurve(endSize, psSizeCurve);
-
+			newSize = transform.localScale + new Vector3 (.1f, .1f, 0);
+			//animate.AnimateToSize (transform.localScale, newSize, 1f);
 			// TODO(samkern): Figure out an appropriate scaling measure between rift & ghost
+			size += .03f;
 			delay = Random.Range (3.0f, 8.0f);
 			yield return new WaitForSeconds(delay);
 		}
@@ -75,9 +58,7 @@ public class Rift : MonoBehaviour, IRestartObserver, IGhostDeathObserver, IPlaye
 	void OnTriggerEnter2D(Collider2D coll) {
 		if (!activated) {
 			ghostStats.timeOpen = Time.time - timeOpened;
-
-			// For reference, the player is .14 scale
-			ghostStats.size = psRadius;
+			ghostStats.size = size;
 
 			// TODO(samkern): Should we record ALL ghosts killed while the rift is active, or just some? Should they decay over time?
 			// ghosts alive during this rift's time in the level = ghosts currently alive plus ghosts murdered.
