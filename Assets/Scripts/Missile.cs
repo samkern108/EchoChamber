71e1d6950f22dc10dcd8065d5c30fba60cba7651
Sam Kern <samkern108@gmail.com>
2018-01-14 18:07:32 -0800

    Boy do I love me some particle effects.

--- Assets/Scripts/Missile.cs
+++ Assets/Scripts/Missile.cs
@@ -31,7 +31,15 @@ public class Missile : MonoBehaviour {
 		RaycastHit2D hit = Physics2D.Raycast(transform.position, moveVector.normalized, 2f, 1 << LayerMask.NameToLayer("Wall"));
 		if (hit.collider) {
 			GameObject explosion = Instantiate (p_explosion);
-			explosion.transform.position = transform.position - (moveVector.normalized * 2 * GetComponent<PolygonCollider2D>().bounds.size.x/3);
+
+			float size;
+			if (GetComponent <PolygonCollider2D> () != null) {
+				size = GetComponent<PolygonCollider2D> ().bounds.size.x;
+			} else {
+				size = GetComponent<BoxCollider2D> ().bounds.size.x;
+			}
+
+			explosion.transform.position = transform.position - (moveVector.normalized * 2 * size/3);
 
 			Vector2 reflection = Vector2.Reflect (moveVector.normalized, hit.normal);
 			explosion.transform.rotation = Quaternion.LookRotation(reflection,Vector3.up);
